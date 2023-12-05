3db3d1337c01e3adc3fd00f0105df645f422a89f
Deepak <deepak1994@gmail.com>
2023-12-05 14:05:32 +0530

    remove paragraph

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <h2>Hello how are you</h2>
-<<<<<<< HEAD
 <p>hello how are you i am fine and you i am also fine</p>
 <p>hi adding button</p>
 <button>click me</button>
-=======
 <p>helo how are you i am fine and you i am also fine</p>
->>>>>>> main
