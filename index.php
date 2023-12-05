57c78d1155290d4e849b89998d86088b621d982d
Deepak <deepak1994@gmail.com>
2023-12-05 14:12:42 +0530

    adding new paragraph

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <h2>Hello how are you</h2>
 <p>hello how are you i am fine and you i am also fine</p>
-<p>hi adding button</p>
-<button>click me</button>
-<p>helo how are you i am fine and you i am also fine</p>
+<p>hello hii</p>
