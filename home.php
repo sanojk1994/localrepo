aa79001eeeb4aedba6e3e3153adbf2993d3389f8
Deepak <deepak1994@gmail.com>
2023-12-06 10:33:18 +0530

    swaping two variable

--- home.php
+++ home.php
@@ -1,1 +1,12 @@
-<h2>home page of given website</h2>
+<h2>writ a program to print swaping two variable</h2>
+<?php
+ $a =4;
+ $b = 5;
+ $a = a+b;
+ $b = a -b;
+ $a = a -b;
+ echo $a;
+ echo $b;
+ echo "how are you";
+ echo "hello";
+?>
