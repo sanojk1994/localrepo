1f17bd0be11531444e398bf94254a70aef304f1d
Deepak <deepak1994@gmail.com>
2023-12-06 12:28:49 +0530

    array maximun value

--- home.php
+++ home.php
@@ -10,3 +10,23 @@
  echo $b;
  echo '<br>';
 ?>
+<h2>write a program to find max and second max number from array</h2>
+<?php
+$data = array('200', '15','69','122','50','201');
+$data_cont = count($data);
+$max_1 = $max_2 = 0;
+for ($i=0; $i<count($data); $i++) { 
+    if ($data[$i] > $max_1){ 
+        $max_2 = $max_1;
+        $max_1 = $data[$i];
+    }
+     else if ($data[$i] > $max_2 && $data[$i] != $max_2)
+    {
+      $max_2 = $data[$i];
+    }
+   
+}
+echo 'max_1=' .$max_1;
+echo'<br>';
+echo 'max_1='.$max_2;
+?>
